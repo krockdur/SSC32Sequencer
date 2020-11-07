69ad516b0e9f26f62debffd083e5d841345d0035
krockdur <web@fougery.ovh>
2020-11-07 16:16:42 +0100

    .

--- src/com/jf72/ui/ServoUI.java
+++ src/com/jf72/ui/ServoUI.java
@@ -6,17 +6,19 @@ import java.util.concurrent.Flow;
 
 public class ServoUI extends JPanel {
 
-
     private int servoNumber;
+    private final int LOW_PULSE = 500;
+    private final int HIGH_PULSE = 2500;
+    private int maxPulse = HIGH_PULSE;
+    private int minPulse = LOW_PULSE;
 
     public ServoUI(int servoNumber){
         this.servoNumber = servoNumber;
 
-        setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+        //setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         setLayout(new BorderLayout());
-
-
 
         add(createTopPanel(), BorderLayout.NORTH);
         add(createContent(), BorderLayout.CENTER);
@@ -31,15 +33,16 @@ public class ServoUI extends JPanel {
         JPanel contentPanel = new JPanel();
 
         sliderPulse = new JSlider();
-        sliderPulse.setMinimum(0);
-        sliderPulse.setMaximum(3000);
+        sliderPulse.setMinimum(minPulse);
+        sliderPulse.setMaximum(maxPulse);
         sliderPulse.setOrientation(JSlider.HORIZONTAL);
-        sliderPulse.setValue(1500);
+        sliderPulse.setValue((maxPulse + minPulse) / 2);
         sliderPulse.setMinorTickSpacing(200);
         sliderPulse.setMajorTickSpacing(1000);
         sliderPulse.setPaintTicks(true);
         sliderPulse.setPaintLabels(true);
         sliderPulse.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 25));
+
         sliderPulse.addChangeListener(e -> {
             txtPulse.setText(Integer.toString(sliderPulse.getValue()));
         });
@@ -53,13 +56,71 @@ public class ServoUI extends JPanel {
     /***************************************************************************/
     /*      CONTENT       */
     private JTextField txtPulse;
+    private JTextField txtMinPulse;
+    private JTextField txtMaxPulse;
     private JPanel createContent(){
-        JPanel contentPanel = new JPanel();
+        JPanel contentPanel = new JPanel(new GridLayout(3,2));
+
         txtPulse = new JTextField();
         txtPulse.setColumns(5);
         txtPulse.setHorizontalAlignment(JTextField.CENTER);
-        txtPulse.setText("1500");
+        txtPulse.setText(Integer.toString((maxPulse + minPulse) / 2));
+        txtPulse.addActionListener(e -> {
+            int tmp_value = Integer.parseInt(txtPulse.getText());
+            if(tmp_value < minPulse){
+                tmp_value = minPulse;
+                txtPulse.setText(Integer.toString(minPulse));
+            }
+            if(tmp_value > maxPulse){
+                tmp_value = maxPulse;
+                txtPulse.setText(Integer.toString(maxPulse));
+            }
+            sliderPulse.setValue(tmp_value);
+        });
+
+        // LOW END
+        txtMinPulse = new JTextField();
+        txtMinPulse.setColumns(5);
+        txtMinPulse.setHorizontalAlignment(JTextField.CENTER);
+        txtMinPulse.setText(Integer.toString(minPulse));
+        txtMinPulse.addActionListener(e -> {
+            if(Integer.parseInt(txtMinPulse.getText()) >= LOW_PULSE){
+                minPulse = Integer.parseInt(txtMinPulse.getText());
+                if( Integer.parseInt(txtPulse.getText()) < Integer.parseInt(txtMinPulse.getText()) ){
+                    txtPulse.setText(txtMinPulse.getText());
+                }
+            }
+            else{
+                minPulse = LOW_PULSE;
+                txtMinPulse.setText(Integer.toString(minPulse));
+            }
+        });
+
+        // HIGH END
+        txtMaxPulse = new JTextField();
+        txtMaxPulse.setColumns(5);
+        txtMaxPulse.setHorizontalAlignment(JTextField.CENTER);
+        txtMaxPulse.setText(Integer.toString(maxPulse));
+        txtMaxPulse.addActionListener(e -> {
+            if(Integer.parseInt(txtMaxPulse.getText()) <= HIGH_PULSE){
+                maxPulse = Integer.parseInt(txtMaxPulse.getText());
+                if( Integer.parseInt(txtPulse.getText()) > Integer.parseInt(txtMaxPulse.getText()) ){
+                    txtPulse.setText(txtMaxPulse.getText());
+                }
+            }
+            else{
+                maxPulse = HIGH_PULSE;
+                txtMaxPulse.setText(Integer.toString(maxPulse));
+            }
+        });
+
+        contentPanel.add(new JLabel("min"));
+        contentPanel.add(txtMinPulse);
+        contentPanel.add(new JLabel("current"));
         contentPanel.add(txtPulse);
+        contentPanel.add(new JLabel("max"));
+        contentPanel.add(txtMaxPulse);
+
         return contentPanel;
     }
 
@@ -88,5 +149,13 @@ public class ServoUI extends JPanel {
 
     public JSlider getSliderPulse() {
         return sliderPulse;
+    }
+
+    public JTextField getTxtMinPulse() {
+        return txtMinPulse;
+    }
+
+    public JTextField getTxtMaxPulse() {
+        return txtMaxPulse;
     }
 }
